fc03f9395430e5fc8863c4c7b2ba1f2780c3f861
asadullahak461 <mrkhan.ak461@gmail.com>
2023-10-29 22:42:41 +0500

    not nullable in room done

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -28,13 +28,13 @@ class MainActivity : AppCompatActivity() {
             val age : Float =  binding.ageet.text.toString().toFloat()
             val bmi :Float = 703*weight/(height*height)
             try {
-                val myObj = data(weight=weight,height = height,age=age,bmi = bmi, name = "khan")
+                val myObj = data(weight=weight,height = height,age=age,bmi = bmi, name = "asad")
                 database.dao().insertLogin(myObj)
             }catch (e: Exception){
                 Toast.makeText(this, "Same Name Cannot Be Added", Toast.LENGTH_SHORT).show()
-            }
+//            }
 
             }
         }
 
-    }
+    }}
--- app/src/main/java/com/example/bmicalculator/room/data.kt
+++ app/src/main/java/com/example/bmicalculator/room/data.kt
@@ -1,5 +1,6 @@
 package com.example.bmicalculator.room
 
+import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
