b18bf90fd12e207e46d3d7fcae3963f27cd21fcd
asadullahak461 <mrkhan.ak461@gmail.com>
2023-10-31 23:23:23 +0500

    loader issue fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("kotlin-kapt")
     id("com.google.gms.google-services")
     id("com.google.firebase.crashlytics")
+    id("com.google.firebase.firebase-perf")
 }
 
 android {
@@ -53,6 +54,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-crashlytics:18.5.1")
     implementation("com.google.firebase:firebase-analytics:21.4.0")
+    implementation("com.google.firebase:firebase-perf:20.5.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -64,4 +66,6 @@ dependencies {
 
     // To use Kotlin annotation processing tool (kapt)
     kapt("androidx.room:room-compiler:$room_version")
+
+    implementation ("com.github.ybq:Android-SpinKit:1.4.0")
 }
--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.bmicalculator
 
+import android.R
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.postDelayed
 import com.example.bmicalculator.databinding.ActivityMainBinding
 import com.example.bmicalculator.room.AppDatabase
 import com.example.bmicalculator.room.DatabaseBuilder
 import com.example.bmicalculator.room.data
+import com.github.ybq.android.spinkit.sprite.Sprite
+import com.github.ybq.android.spinkit.style.DoubleBounce
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,19 +29,32 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
+
+
         binding.calculateBMI.setOnClickListener {
-            val weight : Float = binding.weightet.text.toString().toFloat()
-            val height : Float = binding.heightet.text.toString().toFloat()
-            val age : Float =  binding.ageet.text.toString().toFloat()
-            val bmi :Float = 703*weight/(height*height)
-            try {
-                val myObj = data(weight=weight,height = height,age=age,bmi = bmi, name = "asad")
-                database.dao().insertLogin(myObj)
-            }catch (e: Exception){
-                Toast.makeText(this, "Same Name Cannot Be Added", Toast.LENGTH_SHORT).show()
-//            }
+            binding.spinKit.visibility = View.VISIBLE
+            val weight: Float = binding.weightet.text.toString().toFloat()
+            val height: Float = binding.heightet.text.toString().toFloat()
+            val age: Float = binding.ageet.text.toString().toFloat()
+            val bmi: Float = 703 * weight / (height * height)
+            Handler(Looper.getMainLooper()).postDelayed(5000) {
+                // Do something
+                try {
+                    val myObj =
+                        data(weight = weight, height = height, age = age, bmi = bmi, name = "asad")
+                    database.dao().insertLogin(myObj)
+
+                    binding.spinKit.visibility = View.GONE
+
+                } catch (e: Exception) {
+                    Toast.makeText(this, "Same Name Cannot Be Added", Toast.LENGTH_SHORT).show()
+                    binding.spinKit.visibility = View.GONE
+                }
 
             }
         }
 
-    }}
+    }
+
+}
